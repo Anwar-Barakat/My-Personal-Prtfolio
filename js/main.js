3feebd9f974649c5719c8a8b6b7fe6877e3ee5eb
Anwar Barakat <brkatanwar0@gmail.com>
2022-03-19 17:04:26 +0200

    add works section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//!================================
+//!======= Add Class when Scrolling :
 function scrollHeader() {
     const header = document.getElementById('header');
 
@@ -7,3 +9,26 @@ function scrollHeader() {
         header.classList.remove('scroll-header');
 }
 window.addEventListener('scroll', scrollHeader);
+
+
+//!================================
+//!======= MUXITUP : 
+let mixerPortfolio = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+
+//!================================
+//!======= Link Active Work :
+const linkWork = document.querySelectorAll('.work__item');
+
+function activeWork() {
+    linkWork.forEach(l => l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+linkWork.forEach(l => l.addEventListener('click', activeWork))
